6b1a170d9a0cfed0715c880b71a6d92866d06e35
easywork01 <pranjal4real@gmail.com>
2024-04-12 23:37:23 +0530

    MenuAPi added CORS fixed

--- server.js
+++ server.js
@@ -39,42 +39,42 @@ app.get("/api/restaurants", async (req, res) => {
 });
 
 // For Menu API
-// app.get("/api/menu", async (req, res) => {
-//   const {
-//     "page-type": page_type,
-//     "complete-menu": complete_menu,
-//     lat,
-//     lng,
-//     submitAction,
-//     restaurantId,
-//   } = req.query;
-//   console.log(req.query);
+app.get("/api/menu", async (req, res) => {
+  const {
+    "page-type": page_type,
+    "complete-menu": complete_menu,
+    lat,
+    lng,
+    submitAction,
+    restaurantId,
+  } = req.query;
+  console.log(req.query);
 
-//   const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
 
-//   await fetch(url, {
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Origin": "*",
-//       "User-Agent":
-//         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-//     },
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       res.json(data);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).send("An error occurred");
-//     });
-// });
+  await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
 
 app.get("/", (req, res) => {
   res.json({ test: "Welcome to proxy server" });
